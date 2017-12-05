1b7c9a3c1a55d8be46d20c31232b2b35d69655d2
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-05 15:59:01 -0800

    adding .env var PORT and starting server in index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 'use script';
+
+require('dotenv').config();
+const server = require('./lib/server');
+
+server.start(process.env.PORT, () => {
+  consolo.log(`Server is running on port ${process.env.PORT}`);
+});
