1fca1c3e50dc95f3a75832811c7c357e2e7f8021
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-05 23:28:52 -0800

    POST working but not passing no 200 status code test

---
+++ __test__/server.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const superagent = require('superagent');
+const server = require('../lib/server');
+const cowsay = require('cowsay');
+
+
+describe('server.js', () => {
+  test('POST request should return with a 200 status code and a body is no error', () => {
+    let bodyToTest = { 'text': 'McGregor'};
+    return superagent.post('http://localhost:3000/api/cowsay')
+      .send(bodyToTest)
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body).toEqual(bodyToTest);
+      });
+  });
+
+  test('POST request should return with a 400 status if there is any error', () => {
+    return superagent.post('http://localhost:3000/api/cowsay')
+      .set({'Content-Type' : 'application/json'})
+      .send('{')
+      .then(response => Promise.reject(response))
+      .catch(response => {
+        expect(response.status).toEqual(400);
+      });
+  });
+
+});
