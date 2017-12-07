047cc1543b19640d8fe7e1f142ba64c89148a34a
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-06 17:38:22 -0800

    trying to get 200 POST test to work

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -2,28 +2,26 @@
 
 const superagent = require('superagent');
 const server = require('../lib/server');
-const cowsay = require('cowsay');
-
 
 describe('server.js', () => {
   test('POST request should return with a 200 status code and a body is no error', () => {
-    let bodyToTest = { 'text': 'McGregor'};
+    let cat = '{"text": "catts"}';
     return superagent.post('http://localhost:3000/api/cowsay')
-      .send(bodyToTest)
+      .send(cat)
       .then(response => {
         expect(response.status).toEqual(200);
-        expect(response.body).toEqual(bodyToTest);
+        expect(JSON.stringify(response.body)).toEqual(cat);
       });
   });
 
-  test('POST request should return with a 400 status if there is any error', () => {
-    return superagent.post('http://localhost:3000/api/cowsay')
-      .set({'Content-Type' : 'application/json'})
-      .send('{')
-      .then(response => Promise.reject(response))
-      .catch(response => {
-        expect(response.status).toEqual(400);
-      });
-  });
+  // test('POST request should return with a 400 status if there is any error', () => {
+  //   return superagent.post('http://localhost:3000/api/cowsay')
+  //     .set({'Content-Type' : 'application/json'})
+  //     .send('{')
+  //     .then(response => Promise.reject(response))
+  //     .catch(response => {
+  //       expect(response.status).toEqual(400);
+  //     });
+  // });
 
 });
