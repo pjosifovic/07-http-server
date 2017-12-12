9235f01dfc91ec5752b939f645260b5795458c0e
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-11 20:47:45 -0800

    POST test passing

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -2,26 +2,27 @@
 
 const superagent = require('superagent');
 const server = require('../lib/server');
+const cowsay = require('cowsay');
 
 describe('server.js', () => {
   test('POST request should return with a 200 status code and a body is no error', () => {
-    let cat = '{"text": "catts"}';
+    let cat = {text: 'cats'};
     return superagent.post('http://localhost:3000/api/cowsay')
       .send(cat)
       .then(response => {
         expect(response.status).toEqual(200);
-        expect(JSON.stringify(response.body)).toEqual(cat);
+        expect(response.body).toEqual({content: cowsay.say({text : 'cats'})});
       });
   });
 
-  // test('POST request should return with a 400 status if there is any error', () => {
-  //   return superagent.post('http://localhost:3000/api/cowsay')
-  //     .set({'Content-Type' : 'application/json'})
-  //     .send('{')
-  //     .then(response => Promise.reject(response))
-  //     .catch(response => {
-  //       expect(response.status).toEqual(400);
-  //     });
-  // });
+  test('POST request should return with a 400 status if there is any error', () => {
+    return superagent.post('http://localhost:3000/api/cowsay')
+      .set({'Content-Type' : 'application/json'})
+      .send('{')
+      .then(response => Promise.reject(response))
+      .catch(response => {
+        expect(response.status).toEqual(400);
+      });
+  });
 
 });
